88fc88747b713b723aa2142f96ef503594511a81
ADWAITHSRK <adwaithkrishna43@gmail.com>
2025-04-08 22:10:58 +0530

    fourth-commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import Signup from './projectPages/Register/Register'
 import Navbar from './projectComponents/Navbar/Navbar'
 import Footer from './projectComponents/Footer/Footer'
 import ChatPage from './projectPages/ChatPage/ChatPage'
+import Sidebar from './projectComponents/Sidebar/Sidebar'
 const App = () => {
   return (
     <div className='flex flex-col bg-white min-h-screen' >
       <Navbar/>
-      <ChatPage/>
-      <Footer/>
+      <Sidebar/>
+     
     </div>
   )
 }
--- frontend/src/projectComponents/Navbar/Navbar.jsx
+++ frontend/src/projectComponents/Navbar/Navbar.jsx
@@ -1,36 +1,44 @@
+import { CircleHelp, Menu, Search, Settings, Grip, SearchIcon, SearchXIcon } from "lucide-react";
+import { Image } from "antd";
+import { ConfigProvider, Typography, Input, Avatar } from "antd";
+import Gmail from "../../assets/gmail.png";
+import "@fontsource/roboto";
 import React from "react";
-import { Avatar, Button, Popover } from "antd";
-import { ChartBarIcon, ChartNetwork, MessageCircleDashed } from "lucide-react";
 
 const Navbar = () => {
-  const user = {
-    name: "Rahul Kumar",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Elon_Musk_Royal_Society_crop.jpg/960px-Elon_Musk_Royal_Society_crop.jpg?20250218152106", 
-  };
-  const PopoverContent = (
-    <div flex flex-col items-center>
-      <Avatar size={64} src={user.image} className="mb-2" />
-      <p className="font-semibold text-lg">{user.name}</p>
-      <Button type="primary" danger className="mt-4 w-full">
-        Logout
-      </Button>
-    </div>
-  );
   return (
-    <nav className="w-full h-18 flex justify-between items-center px-18 py-3 bg-white shadow-md border-b border-gray-200">
-      <div className="flex items-center gap-1">
-        <MessageCircleDashed />
-        <h1 className="text-xl font-bold text-blue-500">ChatVerse</h1>
+    <div className="flex justify-between items-center px-6 py-3 h-18 bg-gray-100">
+      <div className="flex gap-2">
+        <Menu />
+        <img src={Gmail} className="w-6 h-6 rounded-md" />
+        <div className="flex justify-center items-center h-6">
+          <h1 className="  text-[19px] text-[#5f6368] font-normal tracking-wide  ">
+            Gmail
+          </h1>
+        </div>
       </div>
-      <Popover
-        content={PopoverContent}
-        trigger="click"
-        placement="bottomRight"
-        className="cursor-pointer"
-      >
-        <Avatar src={user.image} className="cursor-pointer" />
-      </Popover>
-    </nav>
+
+      <div className="flex items-center bg-gray-200 hover:bg-gray-300 px-4 py-1.5 rounded-full w-[700px] h-[44px]">
+        <Search className="w-6 text-gray-500  mr-2"/>
+        <input
+          type="text"
+          placeholder="Search mail"
+          className="bg-transparent w-full outline-none text-gray-700 placeholder:text-gray-500"
+        />
+      </div>
+
+
+      <div className="flex gap-2 justify-center items-center">
+        <CircleHelp  />
+        <Settings  />
+        <Grip  />
+        <div className="flex justify-center items-center"><Avatar
+          src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+          className="mt-2"
+        /></div>
+        
+      </div>
+    </div>
   );
 };
 
